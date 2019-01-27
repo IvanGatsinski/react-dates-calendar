29a2e5b0b04f62c08d4f70e87edc9468602d81ba
IvanGatsinski <playmodge@gmail.com>
2019-01-27 09:30:33 +0200

    Calendar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGem } from '@fortawesome/free-regular-svg-icons';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { DateRangePicker } from 'react-dates';
+import Moment from 'react-moment';
 
+import './App.css';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      focusedInput: null,
+    };
+  }
+
   render() {
+    //const sdate = JSON.stringify(this.state.startDate);
+    //const edate = JSON.stringify(this.state.endDate);
+
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* <Moment>{sdate} -- {edate}</Moment> */}
+        <DateRangePicker
+          startDateId="startDate"
+          endDateId="endDate"
+          startDatePlaceholderText="From"
+          endDatePlaceholderText="To"
+          showClearDates={true}//
+          showDefaultInputIcon={true}//
+          keepOpenOnDateSelect={true}//
+          readOnly={true}//
+          withPortal={true}//
+          customArrowIcon={<FontAwesomeIcon icon={faGem} />}//
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onDatesChange={({ startDate, endDate }) => { this.setState({ startDate, endDate }) }}
+          focusedInput={this.state.focusedInput}
+          onFocusChange={(focusedInput) => { this.setState({ focusedInput }) }}
+        />
       </div>
     );
   }
